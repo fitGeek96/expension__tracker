74f4d1e89f8302029edd3e33fd5da3724c3979e7
fitgeek96 <fitgeek96@gmail.com>
2021-01-20 10:57:09 +0100

    display balance, income & expense

--- app.js
+++ app.js
@@ -50,11 +50,34 @@ function addTransaction(transaction) {
     listEl.appendChild(item);
 }
 
+// UPDATE THE BALANCE, INCOME AND EXPENSE
+function updateValues() {
+    const amounts = transaction.map(tran => tran.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const expense = amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => (acc -= item), 0)
+        .toFixed(2);
+
+    balanceEl.innerText = `$${total}`;
+    moneyPlusEl.innerText = `$${income}`;
+    moneyMinusEl.innerText = `$${expense}`;
+
+}
+
 // INIT APP 
 
-function init(){
+function init() {
     listEl.innerHTML = '';
     transaction.forEach(addTransaction);
+    updateValues();
 }
 
 init();
